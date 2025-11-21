0bcef0ca1ca689b8319a9b74b33febdffe62823b
patheGobel <pathegba@gmail.com>
2025-11-21 10:28:00 +0000

    Test 5

--- docs/data.js
+++ docs/data.js
@@ -51,6 +51,7 @@ const notes = [
     { id: 1, eleve_id: 1, matiere_id: 1, type_evaluation: "test1", note: 3, coefficient: 1, date_evaluation: "2025-11-05", commentaire: "Bon travail ", note_sur: 5 },
     { id: 2, eleve_id: 1, matiere_id: 1, type_evaluation: "test2", note: 2, coefficient: 1, date_evaluation: "2025-11-07", commentaire: "Insuffisant", note_sur: 5 },
     { id: 3, eleve_id: 1, matiere_id: 1, type_evaluation: "test3", note: 3, coefficient: 1, date_evaluation: "2025-11-14", commentaire: "passable", note_sur: 5 },
+    { id: 4, eleve_id: 1, matiere_id: 1, type_evaluation: "test4", note: 3, coefficient: 1, date_evaluation: "2025-11-17", commentaire: "passable", note_sur: 5 },
     //{ id: 3, eleve_id: 1, matiere_id: 1, type_evaluation: "compo", note: 14.0, coefficient: 3, date_evaluation: "2024-03-10", commentaire: "Bonne composition globale" },
     //{ id: 4, eleve_id: 1, matiere_id: 1, type_evaluation: "test", note: 16.0, coefficient: 1, date_evaluation: "2024-01-25", commentaire: "Excellent en algèbre" },
     //{ id: 5, eleve_id: 1, matiere_id: 1, type_evaluation: "devoir", note: 13.5, coefficient: 2, date_evaluation: "2024-02-15", commentaire: "Bon effort sur les probabilités" },
@@ -59,6 +60,7 @@ const notes = [
     { id: 100, eleve_id: 2, matiere_id: 1, type_evaluation: "test", note: 3.5, coefficient: 1, date_evaluation: "2025-11-05", commentaire: "Bon travail", note_sur: 5 },
     { id: 101, eleve_id: 2, matiere_id: 1, type_evaluation: "test", note: 4, coefficient: 1, date_evaluation: "2025-11-07", commentaire: "Bon travail", note_sur: 5 },
     { id: 102, eleve_id: 2, matiere_id: 1, type_evaluation: "test", note: 3, coefficient: 1, date_evaluation: "2025-11-14", commentaire: "passable", note_sur: 5 },
+    { id: 103, eleve_id: 2, matiere_id: 1, type_evaluation: "test", note: 5, coefficient: 1, date_evaluation: "2025-11-17", commentaire: "Très bien", note_sur: 5 },
     //{ id: 8, eleve_id: 2, matiere_id: 1, type_evaluation: "test", note: 12.5, coefficient: 1, date_evaluation: "2024-01-20", commentaire: "Assez bien en algèbre" },
     //{ id: 9, eleve_id: 2, matiere_id: 1, type_evaluation: "compo", note: 14.0, coefficient: 3, date_evaluation: "2024-03-10", commentaire: "Bon travail sur l'ensemble" },
     //{ id: 10, eleve_id: 2, matiere_id: 1, type_evaluation: "devoir", note: 15.0, coefficient: 2, date_evaluation: "2024-02-15", commentaire: "Très bonne maîtrise des concepts" }
@@ -67,14 +69,16 @@ const notes = [
     { id: 200, eleve_id: 3, matiere_id: 1, type_evaluation: "test", note: 2, coefficient: 1, date_evaluation: "2025-11-05", commentaire: "Insuffisant", note_sur: 5 },
     { id: 201, eleve_id: 3, matiere_id: 1, type_evaluation: "test", note: 2, coefficient: 1, date_evaluation: "2025-11-07", commentaire: "Insuffisant", note_sur: 5 },
     { id: 202, eleve_id: 3, matiere_id: 1, type_evaluation: "test", note: 5, coefficient: 1, date_evaluation: "2025-11-14", commentaire: "Très bien", note_sur: 5 },
-
+    { id: 203, eleve_id: 3, matiere_id: 1, type_evaluation: "test", note: 3, coefficient: 1, date_evaluation: "2025-11-17", commentaire: "Passable", note_sur: 5 },
     // Notes pour eleve004 Alhassane Sylla- Tests sur 5, autres sur 20
     { id: 300, eleve_id: 4, matiere_id: 1, type_evaluation: "test1", note: 1.5, coefficient: 1, date_evaluation: "2025-11-05", commentaire: "Insuffisant", note_sur: 5 },
     { id: 301, eleve_id: 4, matiere_id: 1, type_evaluation: "test2", note: 2.5, coefficient: 1, date_evaluation: "2025-11-07", commentaire: "Passable", note_sur: 5 },
     { id: 302, eleve_id: 4, matiere_id: 1, type_evaluation: "test4", note: 3, coefficient: 1, date_evaluation: "2025-11-14", commentaire: "Passable", note_sur: 5 },
+    { id: 303, eleve_id: 4, matiere_id: 1, type_evaluation: "test4", note: 3, coefficient: 1, date_evaluation: "2025-11-17", commentaire: "Passable", note_sur: 5 },
 
     // Notes pour eleve005 Diouma Sow - Tests sur 5, autres sur 20
     { id: 400, eleve_id: 5, matiere_id: 1, type_evaluation: "test", note: 1, coefficient: 1, date_evaluation: "2025-11-05", commentaire: "Insuffisant", note_sur: 5 },
     { id: 401, eleve_id: 5, matiere_id: 1, type_evaluation: "test", note: 0, coefficient: 1, date_evaluation: "2025-11-07", commentaire: "Insuffisant", note_sur: 5 },
     { id: 402, eleve_id: 5, matiere_id: 1, type_evaluation: "test", note: 2, coefficient: 1, date_evaluation: "2025-11-14", commentaire: "passable", note_sur: 5 },
+    { id: 402, eleve_id: 5, matiere_id: 1, type_evaluation: "test", note: 2, coefficient: 1, date_evaluation: "2025-11-17", commentaire: "passable", note_sur: 5 },
 ];
